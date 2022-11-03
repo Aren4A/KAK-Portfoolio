5f99fccbc4ece4e638faad851db016193ec84486
Matu16 <93242535+Matu15@users.noreply.github.com>
2022-11-03 11:44:05 +0200

    alus

--- tutvustus/main.js
+++
@@ -1,8 +0,0 @@
-function burger_menu() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
-}
